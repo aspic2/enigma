deb4e90cbaede92702999aa79c03c685697947c1
MThomp <mthomp@protonmail.com>
2017-12-16 21:29:14 -0600

    cleaning up. OO design

--- enigma/main.py
+++ enigma/main.py
@@ -6,68 +6,77 @@ import string
 # Input a character. Out comes a different character
 # Do this by:
 # 1) Making input uppercase
-# 2) Run input through series of rotors, which change the letter
-# 3) Run changed input through plugboard
+# 2) Run input through plugboard
+# 3) Run input through series of rotors
 # 4) Run input through rotors in reverse order
-# 5) Display final character
+# 5) Run input through plugboard once more
+# 6) Display final character
 
 class Rotor(object):
 
-    def __init__(self, rotor_id):
+    def __init__(self, rotor_id="default"):
         self.id = rotor_id
         self.letters = random.sample(string.ascii_uppercase,
                                      k=len(string.ascii_uppercase))
-        self.current = 0
+        self.current = 1
+        # used to determine how often rotor rotates
+        self.slot = 1
+        self.setting = 1
+
+    def rotate(self):
+        self.current += 1
+        return self
 
     def push_char(self, c):
-        new_char = self.letters[self.current]
-        if self.current == 25:
-            self.current = 0
-        else:
-            self.current += 1
-        if new_char == c:
-            new_char = self.push_char(c)
+        #TODO: abstract this for all rotor slots
+        if self.slot == 1:
+            self.rotate()
+        # This inc's out of index for slots beyond 1.
+        new_char = self.letters[self.current-1]
         return new_char
 
-    def set_rotor(self, setting):
+    def set_rotor(self, slot, setting):
         self.current = setting - 1
+        self.slot = slot
+        return self
+
+
+class Slot(object):
+    """moving logic for spinning the rotor reader here. Facilitates making
+    rotors that rotate at different rates to one another.
+    """
+
+    def __init__(self, position=1):
+        """Only what's important."""
+        self.position = position
+
+    def spin(self):
+        pass
 
 
 class Enigma(object):
 
     def __init__(self):
-        self.rotors = []
+        """Testing with single rotor and single slot"""
+        self.rotors = [Rotor()]
+        self.slots = [Slot(1)]
 
     def encode(self, char):
         # pass message through rotors.
         # rotate first rotor. If first rotor resets to 0, rotate second rotor.
         # if second rotor resets to 0, rotate third rotor
         # etc.
+        for r in self.rotors:
+            char = r.push_char(char)
+        return char
 
-
-
-
-def rotor(c):
-    character_set = random.sample(string.ascii_uppercase, k=len(string.ascii_uppercase))
-    new = character_set.pop(0)
-    # keep full list so cycle can start over
-    character_set.append(new)
-    # letter cannot become itself
-    if new == c:
-        new = character_set.pop(0)
-        character_set.append(new)
-    return new
-
-def process_char(i):
-    i = i.upper()
-    i = rotor(i)
-    return i
 
 def main():
+    enigma = Enigma()
     user_input = None
     while user_input != "end":
         user_input = input("Type next letter or type \"end\"\n> ")
-        print(process_char(user_input))
+        print(enigma.encode(user_input))
     sys.exit(0)
 
 
--- tests/test_main.py
+++ tests/test_main.py
@@ -4,24 +4,19 @@ import string
 from enigma import main
 
 class TestMain(unittest.TestCase):
-
-
-    def test_rotor(self):
-        char = random.choice(string.ascii_uppercase)
-        print(char)
-        transformed_char = main.rotor(char)
-        print(transformed_char)
-        self.assertNotEqual(char, transformed_char)
+    
 
     def test_Rotor(self):
         r = main.Rotor(1)
         char = random.choice(string.ascii_uppercase)
-        self.assertNotEqual(char, r.push_char(char))
         results = []
         # run through all possible combinations
         for n in range(25):
-            results.append(r.push_char(char))
+            new_char = r.push_char(char)
+            results.append(new_char)
         self.assertEqual(len(set(results)), len(results))
+
+
 
 
 
